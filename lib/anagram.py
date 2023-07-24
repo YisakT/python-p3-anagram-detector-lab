f75a9e4a35ba03a28ae59adc5b14b0639c7a870c
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-23 21:40:56 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, potential_matches):
+        matches = []
+        for word in potential_matches:
+            if sorted(word.lower()) == sorted(self.word) and word.lower() != self.word:
+                matches.append(word)
+        return matches
